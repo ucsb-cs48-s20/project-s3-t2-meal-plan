61d12b29e22084a2dffc6ca0e292cc3eddc2e088
ChanChan Mao <chanchanmao@ucsb.edu>
2020-04-24 03:06:07 -0700

    cm - homepage layout

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Layout from "../components/Layout";
 import { optionalAuth } from "../utils/ssr";
 
+import Head from "next/head";
+
 export const getServerSideProps = optionalAuth;
 
 function HomePage(props) {
@@ -8,10 +10,70 @@ function HomePage(props) {
 
   return (
     <Layout user={user}>
+      <Head>
+        <title>Meal Planner</title>
+        {/* title on tab */}
+      </Head>
       {user ? (
         <div>
-          You're logged in! Here's what the server knows about you:
-          <pre>{JSON.stringify(user, null, "\t")}</pre>
+          {/* You're logged in! Here's what the server knows about you:
+          <pre>{JSON.stringify(user, null, "\t")}</pre> */}
+          <h1>Weekly Meal Planner</h1>
+          <table>
+            <tr>
+              <td>
+                <h3>Monday</h3>
+              </td>
+              <td>
+                <h3>Tuesday</h3>
+              </td>
+              <td>
+                <h3>Wednesday</h3>
+              </td>
+              <td>
+                <h3>Thursday</h3>
+              </td>
+              <td>
+                <h3>Friday</h3>
+              </td>
+              <td>
+                <h3>Saturday</h3>
+              </td>
+              <td>
+                <h3>Sunday</h3>
+              </td>
+            </tr>
+            <tr>
+              <td>Breakfast</td>
+              <td>Breakfast</td>
+              <td>Breakfast</td>
+              <td>Breakfast</td>
+              <td>Breakfast</td>
+              <td>Breakfast</td>
+              <td>Breakfast</td>
+            </tr>
+            <br></br>
+            <tr>
+              <td>Lunch</td>
+              <td>Lunch</td>
+              <td>Lunch</td>
+              <td>Lunch</td>
+              <td>Lunch</td>
+              <td>Lunch</td>
+              <td>Lunch</td>
+            </tr>
+            <br></br>
+            <tr>
+              <td>Dinner</td>
+              <td>Dinner</td>
+              <td>Dinner</td>
+              <td>Dinner</td>
+              <td>Dinner</td>
+              <td>Dinner</td>
+              <td>Dinner</td>
+            </tr>
+            <br></br>
+          </table>
         </div>
       ) : (
         <div>
