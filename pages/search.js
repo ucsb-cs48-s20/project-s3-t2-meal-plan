234ee0aadc838a46c9f52070d2b51559b8feffe1
ChanChan Mao <chanchanmao@ucsb.edu>
2020-05-14 20:13:36 -0700

    cm - trying to implement api

--- pages/search.js
+++ pages/search.js
@@ -47,6 +47,18 @@ function Search(props) {
             required
           ></input>
           <button type="submit">Search</button>
+          <br></br>
+          <p>
+            can we make it show that the results show under the search bar or
+            should the page reload with the search results. i think it wud be
+            more efficient if we could show the search results under the search
+            bar so that it's easier for the user to search again for something
+            else.
+            <br></br>
+            <br></br>
+            so on enter: should display title "Search Results:" and results
+            under
+          </p>
         </form>
       </div>
     </Layout>
