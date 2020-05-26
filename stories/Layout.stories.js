912f09a06cbc1c7bea83253294a434d3769a51f5
ChanChan Mao <chanchanmao@ucsb.edu>
2020-05-25 21:40:27 -0700

    cm - storybook runs on localhost

--- stories/Layout.stories.js
+++ stories/Layout.stories.js
@@ -1,15 +1,28 @@
+import React from "react";
+import Layout from "../components/Layout";
 import Container from "react-bootstrap/Container";
-import AppNavbar from "./AppNavbar";
+import { select, text } from "@storybook/addon-knobs";
 
-function Layout(props) {
-  const user = props.user;
+export default {
+  title: "Layout",
+  component: Layout,
+};
 
-  return (
-    <>
-      <AppNavbar user={user} />
-      <Container>{props.children}</Container>
-    </>
+export const loggedOutEmpty = () => {
+  return <Layout />;
+};
+
+export const loggedInWithContentInContainer = () => {
+  const content = text("Sample Content", "This is sample content");
+  const name = text("Name", "Phill Conrad");
+  const picture = text(
+    "Image URL",
+    "https://avatars3.githubusercontent.com/u/1119017"
   );
-}
-
-export default Layout;
+  const user = { name, picture };
+  return (
+    <Layout user={user}>
+      <Container className="py-3">{content}</Container>
+    </Layout>
+  );
+};
