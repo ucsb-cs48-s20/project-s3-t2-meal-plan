1338f1ac9041698b10e9c333eedc22f098b0e2e5
arunryu <arun_ryu@umail.ucsb.edu>
2020-06-02 09:12:22 -0700

    Test02 first try

---
+++ cypress/integration/home.spec.js
@@ -0,0 +1,11 @@
+describe("Home Page", () => {
+  beforeEach(() => {
+    // runs before each test in the block
+    cy.visit("http://localhost:3000");
+  });
+
+  it("has a nav bar", () => {
+    // a nav element with class navbar
+    cy.get("title").should("have.text", "Meal Planner");
+  });
+});
