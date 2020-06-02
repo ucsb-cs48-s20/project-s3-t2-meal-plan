9a0372e1a0de7bf3e9bb64b06094e570aee35ecd
arunryu <arun_ryu@umail.ucsb.edu>
2020-06-02 11:27:46 -0700

    add button testing

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -1,6 +1,7 @@
 describe("Home Page", () => {
   beforeEach(() => {
     // runs before each test in the block
+    cy.loginAsUser();
     cy.visit("http://localhost:3000");
   });
 
@@ -10,17 +11,26 @@ describe("Home Page", () => {
   });
 
   it("has an links in nav bar says Meal Planner", () => {
-    cy.get("nav").its(0).should("have.text", "Meal Planner");
+    cy.get("navh1").should("have.text", "Meal Planner");
   });
 
   it("has links in nav bar says Search for recipes", () => {
-    cy.get("nav").its(1).should("have.text", "Login");
+    cy.get("nav").its(0).should("have.text", "Search for Recipes");
   });
 
-  it("has a message that user did not login", () => {
-    cy.get("message").should(
-      "have.text",
-      "You're not logged in !Login to see your personal meal planner!"
-    );
+  it("has links in nav bar says shoppinglist ", () => {
+    cy.get("nav").its(1).should("have.text", "Shopping List");
+  });
+
+  it("has links in nav bar says shoppinglist ", () => {
+    cy.get("h1").should("have.text", "Weekly Meal Planner");
+  });
+
+  it("has a button to enter meal", () => {
+    cy.get("button").should("have.text", "Enter Meal");
+  });
+
+  it("has a button to clear all meall", () => {
+    cy.get("button").should("have.text", "Clear all");
   });
 });
