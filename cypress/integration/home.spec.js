e7d08de8d3ddea36fba8d6b560ab47be114c09ec
arunryu <arun_ryu@umail.ucsb.edu>
2020-06-02 11:49:09 -0700

    final change

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -4,19 +4,20 @@ describe("Home Page", () => {
     cy.visit("http://localhost:3000");
   });
 
+  it("has a nav bar", () => {
+    // a nav element with class navbar
+    cy.get("nav.navbar").should("exist");
+  });
+
   it("has an links in nav bar says Meal Planner", () => {
-    cy.get("h1").should("have.text", "Meal Planner");
+    cy.get("a").its(0).should("have.text", "Meal Planner");
   });
 
   it("has links in nav bar says Search for recipes", () => {
-    cy.get("a").its(0).should("have.text", "Search for Recipes");
+    cy.get("a").its(1).should("have.text", "Search for Recipes");
   });
 
   it("has links in nav bar says shoppinglist ", () => {
-    cy.get("a").its(1).should("have.text", "Shopping List");
-  });
-
-  it("on homepage there is title said Weekly Meal Planner ", () => {
-    cy.get("h1").should("have.text", "Weekly Meal Planner");
+    cy.get("a").its(2).should("have.text", "Shopping List");
   });
 });
