3d08af03a4c5ec636dd83e7f82b1afe2ccc8dd8b
arunryu <arun_ryu@umail.ucsb.edu>
2020-06-02 11:07:05 -0700

    deleted userlogin testing

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -1,33 +1,23 @@
 describe("Home Page", () => {
-  context("When User logged in", () => {
-    beforeEach(() => {
-      cy.loginAsUser();
-      cy.visit("http://localhost:3000");
-    });
-
-    it("has a nav bar", () => {
-      // a nav element with class navbar
-      cy.get("nav.navbar").should("exist");
-    });
-
-    it("has an links in nav bar says Meal Planner", () => {
-      cy.get("a").its(0).should("have.text", "Meal Planner");
-    });
-
-    it("has links in nav bar says Search for recipes", () => {
-      cy.get("a").its(1).should("have.text", "Search for Recipes");
-    });
-
-    it("has links in nav bar says Shopping lists", () => {
-      cy.get("a").its(2).should("have.text", "Shopping List");
-    });
+  beforeEach(() => {
+    // runs before each test in the block
+    cy.visit("http://localhost:3000");
   });
 
-  context("When User did not logged in", () => {
-    it("has a login button", () => {
-      cy.visit("http://localhost:3000");
+  it("has a nav bar", () => {
+    // a nav element with class navbar
+    cy.get("nav.navbar").should("exist");
+  });
 
-      cy.get("[data-cy=login]").should("exist");
-    });
+  it("has an links in nav bar says Meal Planner", () => {
+    cy.get("a").its(0).should("have.text", "Meal Planner");
+  });
+
+  it("has links in nav bar says Search for recipes", () => {
+    cy.get("a").its(1).should("have.text", "Search for Recipes");
+  });
+
+  it("has links in nav bar says Shopping lists", () => {
+    cy.get("a").its(2).should("have.text", "Shopping List");
   });
 });
