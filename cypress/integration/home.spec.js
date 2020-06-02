906e752f3eb566e6ae94e2b59784736982eda974
arunryu <arun_ryu@umail.ucsb.edu>
2020-06-02 11:00:33 -0700

    update uselogin testing

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -1,24 +1,26 @@
 describe("Home Page", () => {
-  beforeEach(() => {
-    // runs before each test in the block
-    cy.visit("http://localhost:3000");
-  });
+  context("When User logged in", () => {
+    beforeEach(() => {
+      cy.loginAsUser();
+      cy.visit("http://localhost:3000");
+    });
 
-  it("has a nav bar", () => {
-    // a nav element with class navbar
-    cy.get("nav.navbar").should("exist");
-  });
+    it("has a nav bar", () => {
+      // a nav element with class navbar
+      cy.get("nav.navbar").should("exist");
+    });
 
-  it("has an links in nav bar says Meal Planner", () => {
-    cy.get("a").should("have.text", "Meal Planner");
-  });
+    it("has an links in nav bar says Meal Planner", () => {
+      cy.get("a").its(0).should("have.text", "Meal Planner");
+    });
 
-  it("has links in nav bar says Search for recipes", () => {
-    cy.get("a").its(0).should("have.text", "Search for Recipes");
-  });
+    it("has links in nav bar says Search for recipes", () => {
+      cy.get("a").its(1).should("have.text", "Search for Recipes");
+    });
 
-  it("has links in nav bar says Shopping lists", () => {
-    cy.get("a").its(1).should("have.text", "Shopping List");
+    it("has links in nav bar says Shopping lists", () => {
+      cy.get("a").its(2).should("have.text", "Shopping List");
+    });
   });
 
   context("When User did not logged in", () => {
